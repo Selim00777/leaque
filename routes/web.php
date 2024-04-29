fc3ee1dfe9a81c38f7a073f93601cf0deb31fce8
Selim00777 <ggg327626@gmail.com>
2024-04-29 16:24:43 +0500

    <1.9.0>

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $comments=\App\Models\Comment::all();
+    return view('index',compact('comments'));
+});
+Route::get('/', function () {
+    $games=\App\Models\Game::all();
+    return view('index',compact('games'));
+});
+Route::get('/', function () {
+    $players=\App\Models\Player::all();
+    return view('index',compact('players'));
+});
+Route::get('/', function () {
+    $scores=\App\Models\Score::all();
+    return view('index',compact('scores'));
+});
+Route::get('/', function () {
+    $teams=\App\Models\Team::all();
+    return view('index',compact('teams'));
 });
